e1f17129ef7833157992b078ba99cfd3d40de84f
icegriffinguru <icegriffin1122@gmail.com>
2022-01-26 22:17:44 -0800

    0.0.0
    initial hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,75 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-abi-exporter");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+
+module.exports = {
+	solidity: {
+		compilers: [
+			{
+				version: "0.8.0",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: "0.8.2",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: "0.6.2",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			}
+		],
+	},
+	abiExporter: {
+		path: './abi',
+		runOnCompile: true,
+	},
+	networks: {
+		hardhat : {
+			forking: {
+				url: "https://api.avax.network/ext/bc/C/rpc",
+				chainId: 31337,
+			},
+		},
+		avalanche : {
+			url: "https://api.avax.network/ext/bc/C/rpc",
+			chainId: 43114,
+		},
+		// kovan: {
+		// 	url: API_URL,
+		// 	accounts: [`0x${PRIVATE_KEY}`]
+		// }
+	},
+	// defaultNetwork: 'avalanche',
+};
